e29bb742f4f6ce9d00e6da3dfa43824201256757
HATBE <admin@aarongensetter.ch>
2026-01-11 01:45:34 +0100

    dragging stack

--- src/app/components/card-stack/card-stack.ts
+++ src/app/components/card-stack/card-stack.ts
@@ -1,10 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {PlayCard} from '../play-card/play-card';
 import {Card} from '../../types/card.type';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import ZIndexOnCardDragDirective from '../../directives/ZIndexOnCardDrag.directive';
 
 @Component({
   selector: 'app-card-stack',
-  imports: [PlayCard],
+  imports: [PlayCard, DragDropModule, ZIndexOnCardDragDirective],
   templateUrl: './card-stack.html',
   styleUrl: './card-stack.css',
 })
--- src/app/components/play-card/play-card.ts
+++ src/app/components/play-card/play-card.ts
@@ -3,10 +3,11 @@ import {Component, Input} from '@angular/core';
 import {Card} from '../../types/card.type';
 import {CardFront} from './card-front/card-front';
 import {CardBack} from './card-back/card-back';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-play-card',
-  imports: [CardFront, CardBack],
+  imports: [CardFront, CardBack, DragDropModule],
   templateUrl: './play-card.html',
   styleUrl: './play-card.css',
 })
